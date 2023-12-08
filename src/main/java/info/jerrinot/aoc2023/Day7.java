73f5d29335d0a7424c5885856bf77cf6868f3c6b
Jaromir Hamala <jaromir.hamala@gmail.com>
2023-12-08 09:38:24 +0100

    tweaks

--- src/main/java/info/jerrinot/aoc2023/Day7.java
+++ src/main/java/info/jerrinot/aoc2023/Day7.java
@@ -2,19 +2,21 @@ package info.jerrinot.aoc2023;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class Day7 {
     private static final String TIEBREAKER_POINTS_PART_TWO = "J23456789TQKA";
     private static final String TIEBREAKER_POINTS_PART_ONE = "23456789TJQKA";
     private static final Comparator<HandAndBid> SORTER = Comparator.comparingInt((HandAndBid h) -> h.hand.kind.ordinal())
             .thenComparingInt(h -> h.hand.tieBreakPoints)
-            .thenComparingInt(h -> h.bid)
             .reversed();
+
+    record Hand(Kind kind, int tieBreakPoints){}
+    record HandAndBid(Hand hand, int bid){}
 
     enum Kind {
         FiveOfAKind,
@@ -36,7 +38,7 @@ public class Day7 {
         System.out.println("Part II: " + parseAndScore(strings, Day7::parsePartTwo));
     }
 
-    private static int tieBreakerPoints(String cards, String alphabet) {
+    private static int tieBreakers(String cards, String alphabet) {
         int points = 0;
         int multiplier = 1;
         for (int i = cards.length() - 1; i >= 0; i--) {
@@ -50,107 +52,84 @@ public class Day7 {
     static Hand parsePartOne(String cards) {
         return new Hand(
                 getKindPartOne(cards),
-                tieBreakerPoints(cards, TIEBREAKER_POINTS_PART_ONE)
+                tieBreakers(cards, TIEBREAKER_POINTS_PART_ONE)
         );
     }
 
     static Hand parsePartTwo(String cards) {
         return new Hand(
                 getKindPartTwo(cards),
-                tieBreakerPoints(cards, TIEBREAKER_POINTS_PART_TWO)
+                tieBreakers(cards, TIEBREAKER_POINTS_PART_TWO)
         );
     }
 
     private static int parseAndScore(List<String> strings, Function<String, Hand> handParser) {
-        var hands = new ArrayList<HandAndBid>();
-        for (String s : strings) {
-            String[] split = s.split(" ");
-            var hand = handParser.apply(split[0]);
-            var bid = Integer.parseInt(split[1]);
-            hands.add(new HandAndBid(hand, bid));
-        }
-        hands.sort(SORTER);
-        int score = 0;
-        for (int i = 0; i < hands.size(); i++) {
-            HandAndBid handAndBid = hands.get(i);
-            score += handAndBid.bid * (i + 1);
-        }
-        return score;
+        var hands = strings.stream()
+                .map(s -> s.split(" "))
+                .map(split -> new HandAndBid(handParser.apply(split[0]), Integer.parseInt(split[1])))
+                .sorted(SORTER)
+                .toList();
+        return IntStream.range(0, hands.size())
+                .map(i -> hands.get(i).bid * (i + 1))
+                .sum();
     }
 
     static Kind getKindPartTwo(String hand) {
-        var cards = new HashMap<Character, Integer>();
-        int jokerCount = 0;
-        for (int i = 0; i < hand.length(); i++) {
-            char c = hand.charAt(i);
-            if (c == 'J') {
-                jokerCount++;
-            } else {
-                cards.compute(c, (k, v) -> v == null ? 1 : v + 1);
-            }
-        }
+        var counts = new HashMap<Character, Integer>();
+        int jokerCount = countCardsExcept(hand, counts, 'J');
 
         // simple cases
         if (jokerCount == 5 || jokerCount == 4) {
             return Kind.FiveOfAKind;
         } else if (jokerCount == 0) {
-            return getKindPartOne(hand);
+            return getKindPartOne(counts);
         }
 
-        int maxSameCards = cards.values().stream().mapToInt(i -> i).max().getAsInt();
+        int maxSameCards = counts.values().stream().mapToInt(i -> i).max().getAsInt();
         return switch (maxSameCards) {
             case 4 -> Kind.FiveOfAKind;
             case 3 -> Kind.FullHouse.improveBy(jokerCount);
             case 2 -> {
                 if (jokerCount == 1) {
-                    yield cards.size() == 2 ? Kind.FullHouse : Kind.ThreeOfAKind;
-                } else {
-                    yield Kind.ThreeOfAKind.improveBy(jokerCount);
+                    yield counts.size() == 2 ? Kind.FullHouse : Kind.ThreeOfAKind;
                 }
+                // we have a pair of same cards and 2 or 3 jokers
+                // if we have 2 jokers then we have FourOfAKind.
+                // if we have 3 jokers then we have FiveOfAKind.
+                yield Kind.ThreeOfAKind.improveBy(jokerCount);
             }
-            case 1 -> // all non-joker cards are unique
-                    switch (jokerCount) {
-                        case 1 -> Kind.OnePair;
-                        case 2 -> Kind.ThreeOfAKind;
-                        case 3 -> Kind.FourOfAKind;
-                        default -> throw new AssertionError("wtf?");
-                    };
+            case 1 -> Kind.HighCard.improveBy(jokerCount * 2 - 1);
             default -> throw new AssertionError("wtf?");
         };
     }
 
-    static Kind getKindPartOne(String card) {
-        var counts = new HashMap<Character, Integer>();
-        for (int i = 0; i < card.length(); i++) {
-            char c = card.charAt(i);
-            counts.compute(c, (k, v) -> v == null ? 1 : v + 1);
+    private static int countCardsExcept(String cards, HashMap<Character, Integer> results, char exception) {
+        int exceptionCount = 0;
+        for (int i = 0; i < cards.length(); i++) {
+            char c = cards.charAt(i);
+            if (c == exception) {
+                exceptionCount++;
+            } else {
+                results.compute(c, (k, v) -> v == null ? 1 : v + 1);
+            }
         }
-        return switch (counts.values().stream().mapToInt(i -> i).max().getAsInt()) {
+        return exceptionCount;
+    }
+
+    static Kind getKindPartOne(String hand) {
+        var counts = new HashMap<Character, Integer>();
+        countCardsExcept(hand, counts, '*'); // dummy exception value, we want to count *all* cards
+        return getKindPartOne(counts);
+    }
+
+    private static Kind getKindPartOne(HashMap<Character, Integer> cardCounts) {
+        return switch (cardCounts.values().stream().mapToInt(i -> i).max().getAsInt()) {
             case 5 -> Kind.FiveOfAKind;
             case 4 -> Kind.FourOfAKind;
-            case 3 -> {
-                if (counts.size() == 2) {
-                    yield Kind.FullHouse;
-                }
-                yield Kind.ThreeOfAKind;
-            }
-            case 2 -> {
-                if (counts.size() == 3) {
-                    yield Kind.TwoPairs;
-                }
-                yield Kind.OnePair;
-            }
+            case 3 -> cardCounts.size() == 2 ? Kind.FullHouse : Kind.ThreeOfAKind;
+            case 2 -> cardCounts.size() == 3 ? Kind.TwoPairs : Kind.OnePair;
             case 1 -> Kind.HighCard;
             default -> throw new AssertionError("wtf?");
         };
-    }
-
-
-    record Hand(Kind kind, int tieBreakPoints)  {
-
-    }
-
-    record HandAndBid(Hand hand, int bid) {
-
     }
 }
