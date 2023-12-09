a1375cdd7e66e2c12b1378815c35e5b725f70459
Jaromir Hamala <jaromir.hamala@gmail.com>
2023-12-09 13:31:47 +0100

    tweaks.

--- src/main/java/info/jerrinot/aoc2023/Day9.java
+++ src/main/java/info/jerrinot/aoc2023/Day9.java
@@ -7,31 +7,38 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
+import static java.util.stream.Collectors.toCollection;
+
 public class Day9 {
     public static void main(String[] args) throws Exception {
         List<String> strings = Files.readAllLines(Path.of(Day8.class.getClassLoader().getResource("9.txt").toURI()));
 
         var predictionSum = new Prediction(0, 0);
         for (String s : strings) {
-            var numbers = parseLine(s);
-            predictionSum = predictionSum.add(predictNext(numbers));
+            var seq = parseSequence(s);
+            predictionSum = predictionSum.add(predict(seq));
         }
         System.out.println("Part 1: " + predictionSum.last);
         System.out.println("Part 2: " + predictionSum.first);
     }
 
-    private static Prediction predictNext(List<Integer> numbers) {
+    private static Prediction predict(List<Integer> numbers) {
         int last = 0;
         var firstStack = new Stack<Integer>();
+        List<Integer> nextLine = new ArrayList<>();
         do {
-            List<Integer> nextLine = new ArrayList<>();
             for (int i = 1; i < numbers.size(); i++) {
                 int nextNumber = numbers.get(i) - numbers.get(i - 1);
                 nextLine.add(nextNumber);
             }
             last += numbers.get(numbers.size() - 1);
             firstStack.push(numbers.get(0));
+
+            // swap numbers and nextLine, so we can avoid allocations
+            var tmp = numbers;
+            tmp.clear();
             numbers = nextLine;
+            nextLine = tmp;
         } while (!numbers.stream().allMatch(n -> n == 0));
 
         int first = 0;
@@ -48,9 +55,9 @@ public class Day9 {
         }
     }
 
-    static List<Integer> parseLine(String line) {
+    static List<Integer> parseSequence(String line) {
         return Arrays.stream(line.split(" "))
                 .map(Integer::parseInt)
-                .toList();
+                .collect(toCollection(ArrayList::new)); // we will mutate the list
     }
 }
