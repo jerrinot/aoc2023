422b81aa3cdbb6c76644a86507d181451bd412ce
Jaromir Hamala <jaromir.hamala@gmail.com>
2023-12-09 15:39:01 +0100

    tweaks

--- src/main/java/info/jerrinot/aoc2023/Day9.java
+++ src/main/java/info/jerrinot/aoc2023/Day9.java
@@ -11,24 +11,24 @@ import static java.util.stream.Collectors.toCollection;
 
 public class Day9 {
     public static void main(String[] args) throws Exception {
-        List<String> strings = Files.readAllLines(Path.of(Day8.class.getClassLoader().getResource("9.txt").toURI()));
+        var strings = Files.readAllLines(Path.of(Day8.class.getClassLoader().getResource("9.txt").toURI()));
 
         var predictionSum = new Prediction(0, 0);
         for (String s : strings) {
             var seq = parseSequence(s);
             predictionSum = predictionSum.add(predict(seq));
         }
-        System.out.println("Part 1: " + predictionSum.last);
-        System.out.println("Part 2: " + predictionSum.first);
+        System.out.println("Part 1: " + predictionSum.next);
+        System.out.println("Part 2: " + predictionSum.prev);
     }
 
     private static Prediction predict(List<Integer> numbers) {
-        int last = 0;
-        var firstStack = new Stack<Integer>();
+        int next = 0;
+        var prevStack = new Stack<Integer>();
 
         do {
-            last += numbers.get(numbers.size() - 1);
-            firstStack.push(numbers.get(0));
+            next += numbers.get(numbers.size() - 1);
+            prevStack.push(numbers.get(0));
             for (int i = 1; i < numbers.size(); i++) {
                 int nextNumber = numbers.get(i) - numbers.get(i - 1);
                 numbers.set(i - 1, nextNumber);
@@ -36,17 +36,16 @@ public class Day9 {
             numbers.remove(numbers.size() - 1); // remove last
         } while (!numbers.stream().allMatch(n -> n == 0));
 
-        int first = 0;
-        while (!firstStack.isEmpty()) {
-            int firstNumber = firstStack.pop();
-            first = firstNumber - first;
+        int prev = 0;
+        while (!prevStack.isEmpty()) {
+            prev = prevStack.pop() - prev;
         }
-        return new Prediction(first, last);
+        return new Prediction(prev, next);
     }
 
-    record Prediction(int first, int last) {
+    record Prediction(int prev, int next) {
         Prediction add(Prediction other) {
-            return new Prediction(first + other.first, last + other.last);
+            return new Prediction(prev + other.prev, next + other.next);
         }
     }
 
